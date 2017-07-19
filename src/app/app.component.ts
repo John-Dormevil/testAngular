fca173ac85f025ef4c3481b747f654472d2949e6
John-Weetaker <jjdormevil@gmail.com>
2017-07-19 23:03:20 +0200

    move logic appcomponent to herocomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from './model/Hero';
-import { HeroService } from './service/hero.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './view/app.component.html',
   styleUrls: ['./css/app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   private title: string;
-  private heroes: Hero[];
-  private selectedHero: Hero;
-  private newHero: Hero;
 
-  public constructor(private heroService: HeroService) {
+  public constructor() {
     this.title = 'Tour of Heroes';
-    this.newHero = new Hero(0, '');
   }
 
-
-  ngOnInit(): void {
-    this.heroService.getHeroesSlowly().then(heroes => { this.heroes = heroes});
-  }
   public getTitle(): string {
     return this.title;
-  }
-  public addHero(hero: Hero): void {
-    this.heroes.push(this.newHero);
-    this.newHero = new Hero (0, '');
-  }
-  public getHeroes(): Hero[] {
-    return this.heroes;
-  }
-  public setName(index: number, name: string): void {
-    this.heroes[index].setName(name);
-  }
-  public onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  public getSelectedHero(): Hero {
-    return this.selectedHero;
-  }
-  public getNewHero(): Hero {
-    return this.newHero;
   }
 }
--- src/app/hero.component.ts
+++ src/app/hero.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HeroService } from './service/hero.service';
+import { Hero } from './model/Hero';
 
 @Component({
     selector: 'app-hero',
     templateUrl: './view/hero.component.html',
     styleUrls: ['./css/hero.component.css']
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
+    private heroes: Hero[];
+    private selectedHero: Hero;
+    private newHero: Hero;
 
+    public constructor(private heroService: HeroService) {
+        this.newHero = new Hero(0, '');
+    }
+
+    ngOnInit(): void {
+        this.heroService.getHeroesSlowly().then(heroes => { this.heroes = heroes });
+    }
+    public addHero(hero: Hero): void {
+        this.heroes.push(this.newHero);
+        this.newHero = new Hero(0, '');
+    }
+    public getHeroes(): Hero[] {
+        return this.heroes;
+    }
+    public setName(index: number, name: string): void {
+        this.heroes[index].setName(name);
+    }
+    public onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+    public getSelectedHero(): Hero {
+        return this.selectedHero;
+    }
+    public getNewHero(): Hero {
+        return this.newHero;
+    }
 }
 
